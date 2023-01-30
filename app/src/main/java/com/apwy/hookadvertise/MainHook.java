bb82ae4381d366c4c87b62e6e275378fb0562668
ComputerUltra <113588208+ComputerMax@users.noreply.github.com>
2023-01-31 01:17:42 +0800

    init

--- app/src/main/java/com/apwy/hookadvertise/MainHook.java
+++ app/src/main/java/com/apwy/hookadvertise/MainHook.java
@@ -24,9 +24,22 @@ public class MainHook implements IXposedHookLoadPackage {
             case "com.autonavi.minimap":
                 hookMiniMap();
                 break;
+            case "com.kugou.android":
+                hookKuGou();
+                break;
+            case "ctrip.android.view":
+                hookCtrip();
+                break;
+            case "com.qiyi.video":
+                hookIQiYi();
+                break;
+            case "com.tencent.qqmusic":
+                hookQQMusic();
+                break;
         }
     }
 
+    //百度网盘
     private void hookBaiduDisk() throws Throwable {
         Class<?> aClass = lpparam.classLoader.loadClass("com.baidu.netdisk.ui.advertise.FlashAdvertiseFragment");
         XposedBridge.hookAllMethods(aClass, "onCreateView", new XC_MethodHook() {
@@ -39,6 +52,7 @@ public class MainHook implements IXposedHookLoadPackage {
         });
     }
 
+    //高德地图
     private void hookMiniMap() {
         findAndHookMethod("com.autonavi.minimap.SplashViewManager$b", lpparam.classLoader, "onResult", String.class, new XC_MethodReplacement() {
             @Override
@@ -47,5 +61,41 @@ public class MainHook implements IXposedHookLoadPackage {
                 return null;
             }
         });
+    }
+
+    //酷狗音乐
+    private void hookKuGou() {
+        //冷启动广告
+        findAndHookMethod("com.kugou.android.app.splash.SplashActivity", lpparam.classLoader, "x", new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam methodHookParam) {
+                callMethod(methodHookParam.thisObject, "z");
+                return null;
+            }
+        });
+        findAndHookMethod("com.kugou.framework.musicfees.exemptionad.c", lpparam.classLoader, "a", XC_MethodReplacement.returnConstant(true));
+        //热启动广告
+        findAndHookMethod("com.kugou.android.app.splash.foresplash.b", lpparam.classLoader, "k", XC_MethodReplacement.returnConstant(true));
+    }
+
+    //携程旅行
+    private void hookCtrip() {
+        findAndHookMethod("ctrip.business.splash.CtripSplashActivity", lpparam.classLoader, "prepareSplashAd", new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) {
+                callMethod(param.thisObject, "nextStep");
+                return null;
+            }
+        });
+    }
+
+    //爱奇艺
+    private void hookIQiYi() {
+        findAndHookMethod("org.qiyi.android.video.MainActivity", lpparam.classLoader, "x9", XC_MethodReplacement.returnConstant(false));
+    }
+
+    //qq音乐
+    private void hookQQMusic() {
+        findAndHookMethod("com.tencent.qqmusic.business.splash.thirdpartsplash.i", lpparam.classLoader, "c", XC_MethodReplacement.returnConstant(null));
     }
 }
